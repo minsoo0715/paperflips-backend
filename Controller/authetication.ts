f6fa228f394b61b61ef147675d79ff3618a5b7e7
whiteB <whiteblock0715@gmail.com>
2023-06-28 20:41:35 +0900

    ref: JwtPayLoad 인터페이스 작성 및 인증 관련 리팩토링
    - JwtPayLoad 인터페이스를 정의 및 사용해 PayLoad의 구성을 쉽게 확인할 수 있도록 하였습니다.
    - 직관적이지 않던 파라미터 isAdminFunc를 requiresAdmin으로 변경하였습니다.
    - try 문 내의 불필요한 req.cookies null check 코드를 제거했습니다.
    
    issue: #8

--- Controller/authetication.ts
+++ Controller/authetication.ts
@@ -1,41 +1,30 @@
-import express from 'express'
-import jwt from 'jsonwebtoken';
-import {secretObj} from '../config/jwt' //jwt 비밀키
+import express from "express";
+import jwt from "jsonwebtoken";
+import { secretObj } from "../config/jwt"; //jwt 비밀키
+import { jwtPayLoad } from "../interface";
 
+export function auth(requiresAdmin: boolean) {
+  return (
+    req: express.Request,
+    res: express.Response,
+    next: express.NextFunction
+  ) => {
+    try {
+      const token: string = req.cookies.user;
+      const payload = jwt.verify(token, secretObj.secret) as jwtPayLoad; //토큰 검증
 
-export function auth(isAdminFunc:boolean) {
-    return (req:express.Request,res:express.Response,next:express.NextFunction) => {
-      let token:string;
-      let decode:object|string;
-    
-      if(req.cookies === undefined) {
-        res.send(401).end()
-        return;
+      res.locals.id = payload.id;
+
+      if (requiresAdmin && !payload.admin) {
+          res.status(403).end();
+          return;
       }
-      try{
-        token = req.cookies.user;
-        decode = jwt.verify(token, secretObj.secret); //토큰 검증
-        res.locals.id = JSON.parse(JSON.stringify(decode)).id;
 
-        if(isAdminFunc) {
-          if(!JSON.parse(JSON.stringify(decode)).admin) {
-            res.status(403).end();
-            return;
-          }
-        }
+    } catch (err) {
+      res.status(401).end();
+      return;
+    }
 
-        next();
-      }catch(err) {
-         res.status(401).end()
-         return;
-      }
-  
-  }
-}
-
-
-
-
-const admin_auth = (req:express.Request, res:express.Response, next:express.NextFunction) => {
-
+    next();
+  };
 }
