472fdb6501940b5cd9d187f8a885f62b8f391b59
whiteB <whiteblock0715@gmail.com>
2023-06-30 17:48:37 +0900

    ref: Image 업로드 관련 코드 리팩토링
    - storage_handler.ts가 upload 객체가 아닌 upload.single('img') 미들웨어를 export하도록 변경했습니다.
    - 필요하지 않은 import as 구문을 삭제하였습니다.
    - eslint를 적용하였습니다.
    
    issue: #10

--- Image_Server/storage_handler.ts
+++ Image_Server/storage_handler.ts
@@ -1,19 +1,29 @@
-import multer from 'multer';
+import multer from "multer";
+import express from "express";
 
-import * as express from 'express';
 
-const storage:multer.StorageEngine = multer.diskStorage({  
-    destination: function(req:express.Request, file:Express.Multer.File, callback:(error: Error | null, destination: string) => void) {
-      callback(null, "images/")
-    }, //파일 저장 위치
-    filename (req:express.Request, file:Express.Multer.File, callback:(error: Error | null, destination: string) => void) {
-      callback(null, file.originalname)
-  } //파일 이름 
-  })
-  
-  
-  export const upload:multer.Multer = multer( //업로드 객체 
-    {
-      storage:storage
-    }
-  )
+const storage: multer.StorageEngine = multer.diskStorage({
+  destination: function (
+    req: express.Request,
+    file: Express.Multer.File,
+    callback: (error: Error | null, destination: string) => void
+  ) {
+    callback(null, "images/");
+  }, //파일 저장 위치
+  filename(
+    req: express.Request,
+    file: Express.Multer.File,
+    callback: (error: Error | null, destination: string) => void
+  ) {
+    callback(null, file.originalname);
+  }, //파일 이름
+});
+
+const upload: multer.Multer = multer(
+  //업로드 객체
+  {
+    storage: storage,
+  }
+);
+
+export const uploadImg = () => upload.single("img");
